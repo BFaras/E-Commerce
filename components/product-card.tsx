82a7c016916b0b84f7b0b53514289efa6ed18e67
moham <mohamed.ben-faras@polymtl.ca>
2024-07-24 12:36:28 -0400

    display product with image and price

---
+++ components/product-card.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { Product } from '@/type';
+import  Image  from 'next/image';
+import React from 'react'
+import IconButton from './ui/icon-button';
+import {  Expand, ShoppingCart } from 'lucide-react';
+import Currency from './ui/currency';
+
+interface ProductCarProps {
+    data: Product;
+}
+
+export default function ProductCard({data}:ProductCarProps) {
+  return (
+    <div className="big-white group curosor-pointer rounded-xl border p-3 space-y-4">
+      { /*UImages and action*/ }
+
+        <div className = "aspect-square rounded-xl bg-gray-100 relative">
+            <Image
+            src= {data?.images?.[0]?.url}
+            fill
+            alt="Image"
+            className = "aspect-square object-cover rounded-md">
+            </Image> 
+            <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
+                <div className="flex gap-x-6 justify-center">
+                    <IconButton
+                    onClick={() =>{}}
+                    icon={<Expand size = {20} className='text-gray-600'></Expand>}></IconButton>
+                    <IconButton
+                    onClick={() =>{}}
+                    icon={<ShoppingCart size = {20} className='text-gray-600'></ShoppingCart>}></IconButton>
+                    </div>
+            </div>
+        </div>
+        {/* description*/ }
+        <div>
+            <p className = "font-semibold text-lg">
+                {data.name}
+            </p>
+            <p className = "text-sm text-gray-500">
+                {data.category?.name}
+            </p>
+        </div>
+        {/*Price */}
+        <div className = "flex items-center justify-between">
+            <Currency value = {data?.price}></Currency>
+        </div>
+    </div>
+  )
+}
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,7 @@
 import { Product } from '@/type';
 import React from 'react'
 import NoResult from './ui/no-result';
+import ProductCard from './product-card';
 
 interface ProductListProps {
     title:string;
@@ -14,9 +15,7 @@ export default function ProductList({title,items}:ProductListProps) {
         {items.length === 0 && <NoResult></NoResult>}
         <div className ="grid grid-cols-1 sm:grid-cols-2 md:grids-cols-3 lg:grid-cols-4 gap-4">
             {items.map((item) => (
-                <div key = {item.id}>
-                    {item.name}
-                </div>
+                <ProductCard key = {item.id} data = {item}></ProductCard>
             ))}
         </div>
 
