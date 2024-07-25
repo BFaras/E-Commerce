47d9e50ed725aff71d347975aeb50a3c0119b7ce
moham <mohamed.ben-faras@polymtl.ca>
2024-07-25 12:08:04 -0400

    add cart and fix Ui issues with color and size

--- components/info.tsx
+++ components/info.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { Product } from '@/type'
 import React from 'react'
 import Currency from './ui/currency'
+import Button from './ui/button'
+import { ShoppingCart } from 'lucide-react'
 
 interface InfoProps {
     data:Product
@@ -15,18 +18,27 @@ export default function Info({data}:InfoProps) {
             </p>
         </div>
         <hr className='my-4'></hr>
-        <div className='flex items-center gap-x-4'>
-            <h3 className='font-semibold text-black'>Size:</h3>
-            <div>
-                {data?.size?.name}
+        <div className='flex flex-col gap-y-6'>
+            <div className='flex items-center gap-x-4'>
+                <h3 className='font-semibold text-black'>Size:</h3>
+                <div>
+                    {data?.size?.name}
+                </div>
+            </div>
+            <div className='flex items-center gap-x-4'>
+                <h3 className='font-semibold text-black'>Color:</h3>
+                <div className = "h-6 w-6 rounded-full border border-gray-600"
+                style={{backgroundColor:data?.color.value}}/>
+
             </div>
         </div>
-        <div className='flex items-center gap-x-4'>
-            <h3 className='font-semibold text-black'>Color:</h3>
-            <div className = "h-6 w-6 rounded-full border border-gray-600"
-            style={{backgroundColor:data?.color.value}}/>
-
+        <div className='mt-10 flex items-center gap-x-3'>
+            <Button className='flex items-center gap-x-2'>
+                Add to Cart
+                <ShoppingCart></ShoppingCart>
+            </Button>
         </div>
+    
     </div>
   )
 }
