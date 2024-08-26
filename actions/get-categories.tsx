25ff7e19ab218fbbe3940d40b8121945ab2493a1
moham <mohamed.ben-faras@polymtl.ca>
2024-08-26 01:43:25 -0400

    modify api for getting all stores

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,8 +1,6 @@
 import { Category } from "@/type"
 
-
 const url = `${process.env.NEXT_PUBLIC_URL}/categories`
-
 
 export default async function getCategories(): Promise<Category[]> {
 
--- actions/get-stores.tsx
+++ actions/get-stores.tsx
@@ -1,10 +1,9 @@
-import { Store } from "@/type"
-
-
-const url = `${process.env.NEXT_PUBLIC_URL_CMS}`
-
+import { Store } from "@/type";
 
 export default async function getStores(): Promise<Store[]> {
-  const res = await fetch(url);
-  return res.json()
+  const URL = `${process.env.NEXT_PUBLIC_GO_BACKEND}/stores`;
+
+  const res = await fetch(URL);
+
+  return res.json();
 }
